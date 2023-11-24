e0413bb9f336551ad355bd6629ddaa5d691e4971
Hao <ben2000125@yahoo.com.tw>
2023-11-24 15:36:23 +0800

    feat(footer): 實作待辦事項頁尾元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { TodoComponent } from './todo/todo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, TodoComponent],
+  imports: [HeaderComponent, TodoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
