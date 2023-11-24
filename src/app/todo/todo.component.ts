47cc4047f3bdd0e703a5587c96484df33ba30b3e
Hao <ben2000125@yahoo.com.tw>
2023-11-24 14:32:17 +0800

    feat(todo): 設定待辦事項內容為輸入型屬性

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-todo',
@@ -8,9 +8,11 @@ import { CommonModule } from '@angular/common';
   styleUrl: './todo.component.css',
 })
 export class TodoComponent {
-  content = '待辦事項 A';
+  @Input()
+  content!: string;
 
-  hasFinished = false;
+  @Input()
+  hasFinished!: boolean;
 
   onSetStatus(hasFinished: boolean): void {
     this.hasFinished = hasFinished;
